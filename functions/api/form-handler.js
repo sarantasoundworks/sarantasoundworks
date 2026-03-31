159ff228aeefdd3967bb22c39e762c7104b8c67e
sarantasoundworks <sarantaband@gmail.com>
2026-03-31 15:33:18 +0300

    Update form-handler.js

--- functions/api/form-handler.js
+++ functions/api/form-handler.js
@@ -9,7 +9,7 @@ export async function onRequestPost(context) {
     const res = await fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${context.env.RESEND_API_KEY}`,
+        "Authorization": `Bearer "Authorization": `Bearer re_hwkZUHJV_JzpenXCsmJiQTkLqw8ZEcBXT`,`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
